3944180feda38186157ba314aefc028a141b39ba
Jeroen Knoops <jeroen.knoops@gmail.com>
2013-05-28 10:44:18 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Wecollaborate::Application.routes.draw do
 
-  
+  root to: 'static_pages#home'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -11,8 +12,6 @@ Wecollaborate::Application.routes.draw do
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "authentications#create", registrations: "registrations"}
   ActiveAdmin.routes(self)
-
-  root to: 'static_pages#home'
 
   match '/about-creative-commons-policy' => 'static_pages#about_creative_commons_policy', as: 'about_cpp'
   match '/terms-and-conditions' => 'static_pages#terms_and_conditions', as: 'terms'
