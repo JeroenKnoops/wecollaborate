7fca39eb8edacc70a61e45a9fc480e7fbf115890
Jeroen Knoops <jeroen.knoops@gmail.com>
2013-06-02 13:33:47 +0200

    started with the minimal version of the site for the presentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   protect_from_forgery
+  
+  before_filter :set_mode
+  
+  private
+  
+  def set_mode
+    if params[:mode]
+      if params[:mode] == "full"
+        cookies[:mode] = "full"
+        cookies[:mode] = { :value => "full", :expires => 2.hours.from_now }
+      else
+        cookies.delete :mode
+      end
+    end
+    @mode = cookies[:mode] ? :full : :normal
+  end
 end
