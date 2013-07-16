eba56dec6a729cab16ad77def7385555fd2b75cc
Jeroen Knoops <jeroen.knoops@gmail.com>
2013-07-16 22:43:04 +0200

    Removed precompiled assets.. added gists from @uhlenbrock to do precompile locally.

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,3 +80,16 @@ before "deploy:cold",
     "deploy:install_bundler"
 
 after "deploy:update", "newrelic:notice_deployment"
+
+load 'deploy/assets'
+ 
+namespace :deploy do
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      %x{bundle exec rake assets:precompile}
+      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+      %x{bundle exec rake assets:clean}
+    end
+  end
+end
